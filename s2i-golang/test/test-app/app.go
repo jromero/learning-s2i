af1c96af563eba9f18cf2678d6eee04dec6172af
Javier Romero <root@jromero.codes>
2020-07-21 15:17:22 -0500

    Cache working

--- s2i-golang/test/test-app/app.go
+++ s2i-golang/test/test-app/app.go
@@ -2,25 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
-	
+
 	"github.com/gorilla/mux"
 )
 
+const port = "8080"
+
 func main() {
+	log.Println("Starting app on port: ", port)
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "This is a test app!")
+		_, _ = fmt.Fprint(w, "This is a test app!")
 	})
 	r.HandleFunc("/host", func(w http.ResponseWriter, r *http.Request) {
 		var name, _ = os.Hostname()
-		fmt.Fprintf(w, "This request was processed by host: %s\n", name)
+		_, _ = fmt.Fprintf(w, "This request was processed by host: %s\n", name)
 	})
 	r.HandleFunc("/hello/{object}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
-		fmt.Fprintf(w, "Hello %v!\n", vars["object"])
+		_, _ = fmt.Fprintf(w, "Hello %v!\n", vars["object"])
 	})
 	http.Handle("/", r)
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Println("Shutting down")
 }
